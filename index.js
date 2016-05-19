38a330f4880573f66035172f89f6ec4096f3f31d
lightbulbcat <lightbulbcat@icloud.com>
2016-05-20 01:37:25 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// load configs
+
+const jsonfile = require('jsonfile');
+const config = jsonfile.readFileSync('config.json');
+
+// set clients
+
+const Twitter = require('twitter');
+const tw_client = new Twitter(config.twitter);
+
+const elasticsearch = require('elasticsearch');
+const es_client = elasticsearch.Client(config.elasticsearch);
+
+// send queries
+
+// before this, elasticsearch should be have a index 'twitter'.
+// to make it, ex) curl -XPUT localhost:9200/twitter/
+
+const params = {screen_name: 'LightbulbCat'};
+tw_client.get('statuses/user_timeline', params, function(error, tweets, response){
+
+  if (error) {
+    console.log('tw_client error:', error);
+    return;
+  }
+
+  console.log(tweets);
+
+  for(let tweet of tweets) {
+    es_client.index({
+      index: 'twitter',
+      type: 'defalut',
+      id: tweets[0].id,
+      body: tweets[0]
+    });
+  }
+});
