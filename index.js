a6520664eda5744bfc1eeee6d2372ca9b33135c8
lightbulbcat <lightbulbcat@icloud.com>
2016-05-21 14:01:53 +0900

    first promised

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const config = jsonfile.readFileSync('config.json');
 const Twitter = require('twitter');
 const tw_client = new Twitter(config.twitter);
 
+const promised_getTweet = function (params) {
+  return new Promise( function(resolve, reject) {
+    tw_client.get('statuses/user_timeline', params, function(error, tweets, response){
+      if (error) reject (error);
+      resolve(tweets);
+    });
+  });
+};
+
 const elasticsearch = require('elasticsearch');
 const es_client = elasticsearch.Client(config.elasticsearch);
 
@@ -19,21 +28,22 @@ const es_client = elasticsearch.Client(config.elasticsearch);
 // to make it, ex) curl -XPUT localhost:9200/twitter/
 
 const params = {screen_name: 'LightbulbCat'};
-tw_client.get('statuses/user_timeline', params, function(error, tweets, response){
+promised_getTweet(params)
 
-  if (error) {
+  .then( function(tweets) {
+    console.log(tweets);
+
+    for(let tweet of tweets) {
+      es_client.index({
+        index: 'twitter',
+        type: 'defalut',
+        id: tweet.id,
+        body: tweet,
+      });
+    }
+  })
+
+  .catch( function(error) {
     console.log('tw_client error:', error);
     return;
-  }
-
-  console.log(tweets);
-
-  for(let tweet of tweets) {
-    es_client.index({
-      index: 'twitter',
-      type: 'defalut',
-      id: tweet.id,
-      body: tweet,
-    });
-  }
-});
+  })
